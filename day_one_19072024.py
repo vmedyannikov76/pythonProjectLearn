a811038c575c2fe27516cfc13b90b81ad4103d5e
Vladimir Medyannikov <vmedyannikov76@gmail.com>
2024-07-20 18:25:37 +0300

    days two

--- day_one_19072024.py
+++ day_one_19072024.py
@@ -1,1 +1,13 @@
-# Иннициализируем репозиторий
+spells = [
+    "Riddikulus!",
+    "Wingardium Leviosa",
+    "Avada Kedavra",
+    "Expecto Patronum",
+    "Nox!",
+    "Lumos!",
+]
+disaster = True
+if disaster:
+    print('Да')
+else:
+    print("НЕТ")
